50370f9dbd0d7085ededcfccfa0bcf11780d0507
Christopher Martinez Demmans <132075292+Christopher-DSA@users.noreply.github.com>
2023-10-24 21:44:49 -0400

    Update Creating Table for data_offers.sql

--- Creating Table for data_offers.sql
+++ Creating Table for data_offers.sql
@@ -1,5 +1,5 @@
 USE Gett_Taxi_Database
-CREATE TABLE data_offers(order_gk BIGINT PRIMARY KEY unique,
+CREATE TABLE data_offers(order_gk BIGINT PRIMARY KEY UNIQUE,
 						order_time CHAR(11),
 						origin_longitude DECIMAL(10,6),
 						origin_latitude DECIMAL(9,6),
